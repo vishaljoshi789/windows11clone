4936829281d8bda3189487ea8d47d351478e97ee
Vishal <visjoshi1234@gmail.com>
2021-06-27 21:55:32 +0530

    finally completed

--- js/script.js
+++ js/script.js
@@ -14,11 +14,15 @@ startmenu = document.getElementsByClassName("startmenu")[0]
 startbtn.addEventListener("click", ()=>{
 	if(startmenu.style.bottom== '-650px'){
 		startmenu.style.bottom = '60px'
+		search.style.bottom='-200px'
+		notepadinput.style.bottom = '-700px'
 	}else{
 		startmenu.style.bottom = '-650px'
 	}
 
 })
+
+// clickable file manager
 file = document.getElementById("fileExplore");
 file.addEventListener("click", ()=>{
 	window.open('file:///C:');
@@ -34,6 +38,8 @@ searchbtn = document.getElementById("searchbtn");
 searchbtn.addEventListener("click", ()=>{
 	if(search.style.bottom=='-200px'){
 		search.style.bottom='60px'
+		startmenu.style.bottom = '-650px'
+		notepadinput.style.bottom = '-700px'
 	}else{
 		search.style.bottom='-200px'
 	}
@@ -54,10 +60,39 @@ calander.addEventListener("click", ()=>{
 // clickable notepad
 notepad = document.getElementById("notepad")
 notepadinput = document.getElementById("notepadinput")
+notepadtaskbar = document.getElementById("notepadtaskbar");
 notepad.addEventListener("click", ()=>{
-	if(notepadinput.style.display=='none'){
-		notepadinput.style.display = "inline"
+	if(notepadinput.style.bottom=='-700px'){
+		notepadinput.style.bottom = "70px"
+		startmenu.style.bottom = '-650px'
+		search.style.bottom='-200px'
+		notepadtaskbar.style.display = 'inline'
 	}else{
-		notepadinput.style.display = 'none'
+		notepadinput.style.bottom = '-700px'
 	}
 })
+notepadtaskbar.addEventListener("click", ()=>{
+	if(notepadinput.style.bottom=='-700px'){
+		notepadinput.style.bottom = "70px"
+		startmenu.style.bottom = '-650px'
+		search.style.bottom='-200px'
+		notepadtaskbar.style.display = 'inline'
+	}else{
+		notepadinput.style.bottom = '-700px'
+	}
+})
+
+
+function savingnotepad(){
+    var text = document.getElementById("savenote").value;
+    text = text.replace(/\n/g, "\r\n"); // To retain the Line breaks.
+    var blob = new Blob([text], { type: "text/plain"});
+    var anchor = document.createElement("a");
+    anchor.download = "my-note.txt";
+    anchor.href = window.URL.createObjectURL(blob);
+    anchor.target ="_blank";
+    anchor.style.display = "none"; // just to be safe!
+    document.body.appendChild(anchor);
+    anchor.click();
+    document.body.removeChild(anchor);
+ }
